a18b38505907393cc2514011c8a002b67fa3adfc
Thejaswi <thejaswirl187@gmail.com>
2022-05-05 17:59:24 +0530

    Added: Employees Wage is calaculated

---
+++ Day_9_EmployeeWage/src/com/bridgelabz_employeewage/Main.java
@@ -0,0 +1,36 @@
+package com.bridgelabz.employeewage;
+import java.util.Random;
+public class Main {
+
+    public static final int IS_FULL_TIME = 1;
+    public static final int IS_PART_TIME = 2;
+    public static final int EMP_RATE_PER_HOUR = 20;
+    public static int empHrs = 0;
+    public static int totalEmpHrs = 0;
+    public static int numOfWorkingDays = 20;
+
+    private static void wageComputation() {
+        Random random = new Random();
+        for (int i = 0; i < numOfWorkingDays; i++) {
+            int empCheck = (int) Math.floor(Math.random() * 10) % 3;
+            switch (empCheck) {
+                case IS_FULL_TIME:
+                    System.out.println("Employee is Present Full Time");
+                    empHrs = 8;
+                    break;
+                case IS_PART_TIME:
+                    System.out.println("Employee is Present Part Time ");
+                    empHrs = 4;
+                    break;
+                default:
+            }
+            totalEmpHrs = totalEmpHrs + empHrs;
+        }
+        int empWage = totalEmpHrs * EMP_RATE_PER_HOUR;
+        System.out.println("Employee Wage is : " + empWage);
+    }
+    public static void main(String[] args) {
+        System.out.println("Welcome to Employee Wage Computation Program on Master Branch");
+        wageComputation();
+    }
+}
