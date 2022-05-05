6c68dba16033c23ed6b516c5bddbd44e069c1bb6
Thejaswi <thejaswirl187@gmail.com>
2022-05-05 18:23:52 +0530

    Added: Employees Wage calculation

--- Day_9_EmployeeWage/src/com/bridgelabz_employeewage/Main.java
+++ Day_9_EmployeeWage/src/com/bridgelabz_employeewage/Main.java
@@ -6,12 +6,13 @@ public class Main {
     public static final int IS_PART_TIME = 2;
     public static final int EMP_RATE_PER_HOUR = 20;
     public static final int MAX_HRS_IN_MONTHS = 100;
-    public static int empHrs = 0 ,  totalWorkingDays = 0, totalEmpHrs = 0;
+    public static int empHrs = 0, totalWorkingDays = 0, totalEmpHrs = 0;
     public static int numOfWorkingDays = 20;
 
     private static void wageComputation() {
         Random random = new Random();
-        while ( totalEmpHrs < MAX_HRS_IN_MONTHS && totalWorkingDays < numOfWorkingDays ) {
+        while (totalEmpHrs < MAX_HRS_IN_MONTHS && totalWorkingDays < numOfWorkingDays) {
+            totalWorkingDays++;
             int empCheck = (int) Math.floor(Math.random() * 10) % 3;
             switch (empCheck) {
                 case IS_FULL_TIME:
@@ -22,15 +23,17 @@ public class Main {
                     System.out.println("Employee is Present Part Time ");
                     empHrs = 4;
                     break;
-                default :
+                default:
+                    empHrs = 0;
             }
             totalEmpHrs = totalEmpHrs + empHrs;
         }
-        int empWage = totalEmpHrs * EMP_RATE_PER_HOUR;
-        System.out.println("Employee Wage is : " + empWage);
+        int totalEmpWage = totalEmpHrs * EMP_RATE_PER_HOUR;
     }
+
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage Computation Program on Master Branch");
         wageComputation();
+
     }
 }
